17bcfe3260d126de9fddffa6d500b8c1d14149e9
NDUB <ndubetz@pillartechnology.com>
2017-04-11 12:30:52 -0400

    started test driving graph builder

--- src/scheduler.unit.js
+++ src/scheduler.unit.js
@@ -1,14 +1,14 @@
 const scheduler = require('./scheduler');
 
 describe('Scheduler', () => {
-  it('produces one pairing combinations from a list of two', () => {
+  it('produces one pairing combination from a list of two', () => {
     const developers = ['Alice', 'Bob'];
     const pairings = ['Alice/Bob'];
     expect(scheduler(developers)).toEqual(pairings);
   });
-  // it('produces all pairing combinations from a list of four', () => {
-  //   const developers = ['Alice', 'Bob', 'Cathy', 'Dan'];
-  //   const pairings = ['Alice/Bob,Cathy/Dan', 'Alice/Cathy,Bob/Dan', 'Alice/Dan,Bob/Cathy'];
-  //   expect(scheduler(developers)).toEqual(pairings);
-  // });
+  xit('produces three unique pairing combinations from a list of four', () => {
+    const developers = ['Alice', 'Bob', 'Cathy', 'Dan'];
+    const pairings = ['Alice/Bob,Cathy/Dan', 'Alice/Cathy,Bob/Dan', 'Alice/Dan,Bob/Cathy'];
+    expect(scheduler(developers)).toEqual(pairings);
+  });
 });
