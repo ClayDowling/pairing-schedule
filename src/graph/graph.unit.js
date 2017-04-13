fdeda4a7ed7e691ed89bd24f68c71d7f4a7a8535
NDUB <ndubetz@pillartechnology.com>
2017-04-12 23:01:05 -0400

    added eslint and stuff that ship-it fails to add

---
+++ src/graph/graph.unit.js
@@ -0,0 +1,42 @@
+var buildGraph = require('./graph');
+
+describe('Graph builder', () => {
+  it('constructs a complete graph with 2 nodes and 1 edge', () => {
+    const nodes = ['Alice', 'Bob'];
+    const expectedGraph = [{ v1: 'Alice', v2: 'Bob' }];
+    expect(buildGraph(nodes)).toEqual(expectedGraph);
+  });
+  it('constructs a complete graph with 4 nodes and 6 edges', () => {
+    const nodes = ['Alice', 'Bob', 'Cathy', 'Dan'];
+    const expectedGraph = [
+      { v1: 'Alice', v2: 'Bob' },
+      { v1: 'Alice', v2: 'Cathy' },
+      { v1: 'Alice', v2: 'Dan' },
+      { v1: 'Bob', v2: 'Cathy' },
+      { v1: 'Bob', v2: 'Dan' },
+      { v1: 'Cathy', v2: 'Dan' }
+    ];
+    expect(buildGraph(nodes)).toEqual(expectedGraph);
+  });
+  it('constructs a complete graph with 6 nodes and 15 edges', () => {
+    const nodes = ['Alice', 'Bob', 'Cathy', 'Dan', 'Ellen', 'Frank'];
+    const expectedGraph = [
+      { v1: 'Alice', v2: 'Bob' },
+      { v1: 'Alice', v2: 'Cathy' },
+      { v1: 'Alice', v2: 'Dan' },
+      { v1: 'Alice', v2: 'Ellen' },
+      { v1: 'Alice', v2: 'Frank' },
+      { v1: 'Bob', v2: 'Cathy' },
+      { v1: 'Bob', v2: 'Dan' },
+      { v1: 'Bob', v2: 'Ellen' },
+      { v1: 'Bob', v2: 'Frank' },
+      { v1: 'Cathy', v2: 'Dan' },
+      { v1: 'Cathy', v2: 'Ellen' },
+      { v1: 'Cathy', v2: 'Frank' },
+      { v1: 'Dan', v2: 'Ellen' },
+      { v1: 'Dan', v2: 'Frank' },
+      { v1: 'Ellen', v2: 'Frank' }
+    ];
+    expect(buildGraph(nodes)).toEqual(expectedGraph);
+  });
+});
