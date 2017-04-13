7b6209518727c0319e393a7da4fb10cc4ef5a9d7
NDUB <ndubetz@pillartechnology.com>
2017-04-13 01:06:49 -0400

    refactor

--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -2,8 +2,7 @@ module.exports = (pairings) => {
   const schedules = [];
   const numberOfPairings = pairings.length;
   for (let i = 0; i < numberOfPairings; i++) {
-    const pairs = [pairings[i].pair];
-    schedules.push(pairs);
+    schedules.push(pairings[i].pair);
   }
   return schedules;
 };
--- src/scheduler/scheduler.unit.js
+++ src/scheduler/scheduler.unit.js
@@ -2,16 +2,18 @@ const buildPairs = require('../build-pairs/build-pairs');
 const scheduler = require('./scheduler');
 
 describe('Scheduler', () => {
-  it('produces one pairing combination from a list of two', () => {
+  it('produces one pairing schedule from one pair', () => {
     const pairs = buildPairs(['Alice', 'Bob']);
+    const expectedSchedules = [
+      'Alice & Bob'
+    ];
+    expect(scheduler(pairs)).toEqual(expectedSchedules);
+  });
+  xit('produces three pairing schedules from six pairs', () => {
+    const pairs = buildPairs(['Alice', 'Bob', 'Cathy', 'Dan']);
     const expectedSchedules = [
       ['Alice & Bob']
     ];
     expect(scheduler(pairs)).toEqual(expectedSchedules);
-  });
-  xit('produces three unique pairing combinations from a list of four', () => {
-    const developers = ['Alice', 'Bob', 'Cathy', 'Dan'];
-    const pairings = ['Alice/Bob,Cathy/Dan', 'Alice/Cathy,Bob/Dan', 'Alice/Dan,Bob/Cathy'];
-    expect(scheduler(developers)).toEqual(pairings);
   });
 });
