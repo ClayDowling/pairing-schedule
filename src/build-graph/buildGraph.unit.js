c2350a3e195c37d9355ac319100d3bef5c3480f4
NDUB <ndubetz@pillartechnology.com>
2017-04-12 22:17:30 -0400

    builds graph with six nodes

--- src/build-graph/buildGraph.unit.js
+++ src/build-graph/buildGraph.unit.js
@@ -31,4 +31,28 @@ describe('Graph builder', () => {
     };
     expect(buildGraph(nodes)).toEqual(expectedGraph);
   });
+  it('constructs a graph with 6 nodes and 15 edges', () => {
+    const nodes = ['Alice', 'Bob', 'Cathy', 'Dan', 'Ellen', 'Frank'];
+    const expectedGraph = {
+      nodes,
+      edges: [
+        { v1: 'Alice', v2: 'Bob' },
+        { v1: 'Alice', v2: 'Cathy' },
+        { v1: 'Alice', v2: 'Dan' },
+        { v1: 'Alice', v2: 'Ellen' },
+        { v1: 'Alice', v2: 'Frank' },
+        { v1: 'Bob', v2: 'Cathy' },
+        { v1: 'Bob', v2: 'Dan' },
+        { v1: 'Bob', v2: 'Ellen' },
+        { v1: 'Bob', v2: 'Frank' },
+        { v1: 'Cathy', v2: 'Dan' },
+        { v1: 'Cathy', v2: 'Ellen' },
+        { v1: 'Cathy', v2: 'Frank' },
+        { v1: 'Dan', v2: 'Ellen' },
+        { v1: 'Dan', v2: 'Frank' },
+        { v1: 'Ellen', v2: 'Frank' }
+      ]
+    };
+    expect(buildGraph(nodes)).toEqual(expectedGraph);
+  });
 });
