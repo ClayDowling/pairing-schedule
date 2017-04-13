211896d8f756f770e07e65cde0f1a41030923cbf
NDUB <ndubetz@pillartechnology.com>
2017-04-12 23:28:12 -0400

    configured eslint and started pairing test

--- jasmine.js
+++ jasmine.js
@@ -2,10 +2,10 @@ const Jasmine = require('jasmine');
 const jasmine = new Jasmine();
 
 jasmine.loadConfig({
-    spec_dir: 'src',
-    spec_files: [
-        '**/*.unit.js'
-    ]
+  spec_dir: 'src',
+  spec_files: [
+    '**/*.unit.js'
+  ]
 });
 
 jasmine.execute();
---
+++ src/pairings/pairings.unit.js
@@ -0,0 +1,5 @@
+const graph = require('../graph');
+
+describe('Pairing builder', () => {
+  it('Builds one pairing given a graph with two nodes', () => {});
+});
